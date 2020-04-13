fdb89a3fc79636f6571a72e3c35397541da377f1
Eric Hu <676307+eric-hu@users.noreply.github.com>
2020-04-14 00:37:43 +0800

    Change homepage and build

---
+++ docs/precache-manifest.4aa5e08a546b43b1a9abf06b92f1c227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8e36b1c0b8071d3f4a2e1c4718986a",
+    "url": "/react-sandbox/docs/index.html"
+  },
+  {
+    "revision": "42be101109c920944d58",
+    "url": "/react-sandbox/docs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6e4706bf3bf5fc4cb81",
+    "url": "/react-sandbox/docs/static/js/2.dd65bab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sandbox/docs/static/js/2.dd65bab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42be101109c920944d58",
+    "url": "/react-sandbox/docs/static/js/main.d72a341e.chunk.js"
+  },
+  {
+    "revision": "fe5f263158112afa2b67",
+    "url": "/react-sandbox/docs/static/js/runtime-main.bfdeb621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sandbox/docs/static/media/logo.5d5d9eef.svg"
+  }
+]);
